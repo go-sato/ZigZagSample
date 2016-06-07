13dfbdfc162c4c8447c8d0191e9558a4812d2678
go <g-sato@colopl.co.jp>
2016-06-07 17:06:28 +0900

    sidebar bug fix.

--- Assets/_Script/PlayerManager.cs
+++ Assets/_Script/PlayerManager.cs
@@ -14,13 +14,13 @@ public class PlayerManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		moveObject = GetComponent<MoveObject> ();
-		mainSpriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		mainSpriteRenderer = GetComponent<SpriteRenderer> ();
 		//transform.rotation = Quaternion.Euler(0, 0, -20);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		Clamp();
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
@@ -50,4 +50,26 @@ public class PlayerManager : MonoBehaviour {
 			Destroy (gameObject);
 		}
 	}
+
+	void Clamp ()
+	{
+		// 画面左下のワールド座標をビューポートから取得
+		Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+		// 画面右上のワールド座標をビューポートから取得
+		Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+		// プレイヤーの座標を取得
+		Vector2 pos = transform.position;
+
+		if (pos.x < min.x || max.x < pos.x) {
+			ChangeDirection ();
+		}
+
+		// プレイヤーの位置が画面内に収まるように制限をかける
+		pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+		pos.y = Mathf.Clamp (pos.y, min.y, max.y);
+
+		// 制限をかけた値をプレイヤーの位置とする
+		transform.position = pos;
+	}
 }
+
--- Assets/_Script/Sidebar.cs
+++ Assets/_Script/Sidebar.cs
@@ -12,11 +12,10 @@ public class Sidebar : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-
 		if (other.gameObject.CompareTag ("Player")) {
 			playerManager.ChangeDirection ();
 		}
